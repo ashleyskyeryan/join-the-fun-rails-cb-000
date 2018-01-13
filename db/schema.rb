fbebd6ce5d48f1d58424afdaeb5dab7765acc79e
Ashley Ryan <ashleyskyeryan@gmail.com>
2018-01-13 21:34:38 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,6 @@ ActiveRecord::Schema.define(version: 20180113212254) do
   create_table "rides", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "taxi_id"
   end
 
   create_table "taxis", force: :cascade do |t|
